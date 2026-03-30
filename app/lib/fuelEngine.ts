b5c495c7e16359e70997fb49d6a054b2137288f5
FloP-maker <florent.poussin@gmail.com>
2026-03-30 18:53:52 +0200

    fix

--- app/lib/fuelEngine.ts
+++ app/lib/fuelEngine.ts
@@ -97,7 +97,7 @@ function determineChoStrategy(
     progressionProfile = CHO_PROGRESSION_PROFILES.conservative;
     warnings.push(
       "Stratégie conservatrice appliquée en raison d'une tolérance GI sensible"
-    ); } else if (profile.giTolerance === "robust") {}
+    ); } else if (profile.giTolerance === "robust") {
     progressionProfile = CHO_PROGRESSION_PROFILES.aggressive;
   }
 
